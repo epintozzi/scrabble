d157f022c72a55462550450f02403591331d6801
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-09-06 13:38:47 -0600

    assessment

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,40 @@
+require "./lib/scrabble"
+
 class Player
+
+  def initialize
+    @total = 0
+    @words = []
+  end
+
+  def play(word)
+    scrabble = Scrabble.new
+    @words << word
+    @total += scrabble.score(word)
+  end
+
+  def total
+    @total
+  end
+
+  def won?
+    if @total > 25
+      return true
+    else
+      false
+    end
+  end
+
+  def words
+    @words
+  end
+
+  def tile_count
+    tile_count = 0
+    @words.each do |word|
+      tile_count += word.length
+    end
+    return tile_count
+  end
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -5,5 +5,59 @@ require 'minitest/pride'
 require 'pry'
 
 class PlayerTest < Minitest::Test
+  def test_player_can_make_word
+    player = Player.new
+
+    assert_equal 8, player.play('hello')
+  end
+
+  def test_it_can_calculate_total
+    player = Player.new
+    player.play("Hello")
+    assert_equal 8, player.total
+    player.play("Hi")
+    assert_equal 13, player.total
+    player.play("snacker")
+    assert_equal 26, player.total
+  end
+
+  def test_if_player_has_won?
+    player = Player.new
+
+    refute player.won?
+  end
+
+  def test_if_play_won_with_25_points
+    player = Player.new
+
+    player.play("Hello")
+    assert_equal 8, player.total
+    player.play("Hi")
+    assert_equal 13, player.total
+    player.play("snacker")
+    assert_equal 26, player.total
+
+    assert player.won?
+  end
+
+  def test_it_returns_array_of_words_played
+    player = Player.new
+
+    player.play('hello')
+    player.play('hi')
+    player.play('snacker')
+
+    assert_equal ['hello', 'hi', 'snacker'], player.words
+  end
+
+  def test_it_counts_tiles
+    player = Player.new
+
+    player.play('hello')
+    player.play('hi')
+    player.play('snacker')
+
+    assert_equal 14, player.tile_count
+  end
 
 end
